5085b4492f88f6171fb7148134653d3f1402f2e1
ssreynoso <s.reynosodunjo@gmail.com>
2025-04-08 15:44:32 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className='container mx-auto h-screen flex items-center justify-center bg-red-500'>
+            <h1 className='text-xl font-bold'>Prueba eze</h1>
+        </div>
+    )
+}
+
+export default App
